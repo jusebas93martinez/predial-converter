a2d49ed702b753bce05cb057ff44e7692a8fe542
Sebastián Martinez <sebas93martinez@gmail.com>
2025-01-09 14:19:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PredialConverter from "./components/PredialConverter";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <PredialConverter />
+    </div>
+  );
+}
+
+export default App;
